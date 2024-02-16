58c92f667c31a524840868356c47b65229bb7497
zibbini <ziadibbini@gmail.com>
2024-02-16 13:53:54 +0000

    Add parameters as own section for ancillary scripts.

--- image_augmentation_gif.py
+++ image_augmentation_gif.py
@@ -18,81 +18,95 @@ import time
 
 # Parameters -----------------------
 limit = 3000 # For removing class imbalances, all individual classes will be augmented until this limit is reached
-ann = pd.read_csv('./annotations_train.csv') # Annotation training dataset produced from 'split_dataset.py'
-im_dir = './video' # Output folder for exporting augmenting GIF files
+ann = pd.read_csv('./annotations_train_3s.csv') # Annotation training dataset produced from 'split_dataset.py'
+im_dir = '/run/media/z/fast1/dev-resnet_video/timepoint_videos' # Output folder for exporting augmenting GIF files
+output_file = './annotations_train_3s_aug.csv' # Output file for annotations of augmented files
+check_class_imbalances = False # Check class imbalances present within training data
 # ----------------------------------
 
-ia.seed(1)
+if check_class_imbalances:
+    print('Event imbalances:')
+    for e, event in ann.groupby('single_event'):
+        print(f'Event: {e}', f'No. samples: {len(event)}')
+else:
+    # Check for existing augmented files
+    print('Removing existing files...')
+    aug_files = glob.glob(f'{im_dir}/*_3s_*.gif')
+    for f in tqdm(aug_files):
+        os.remove(f)
 
-seq = iaa.Sequential([
-	iaa.Sometimes(0.5, iaa.Fliplr()),
-	iaa.Sometimes(0.5, iaa.Flipud()),
-	iaa.OneOf((
-		iaa.Multiply((0.5, 2)),
-		iaa.Salt(0.025),
-		iaa.Pepper(0.025)
-	)),
-	iaa.Sometimes(0.5, iaa.GaussianBlur(sigma=(0, 0.5)))
-], random_order=True)
+    ia.seed(1)
 
-def process(arg):
-	i, fn = arg
+    seq = iaa.Sequential([
+        iaa.Sometimes(0.5, iaa.Fliplr()),
+        iaa.Sometimes(0.5, iaa.Flipud()),
+        iaa.OneOf((
+            iaa.Multiply((0.5, 2)),
+            iaa.Salt(0.025),
+            iaa.Pepper(0.025)
+        )),
+        iaa.Sometimes(0.5, iaa.GaussianBlur(sigma=(0, 0.5)))
+    ], random_order=True)
 
-	ann_aug_batch = {e:[] for e in ann.keys()[1:]}
+    def process(arg):
+        i, fn = arg
 
-	frames = imageio.mimread(fn)
+        ann_aug_batch = {e:[] for e in ann.keys() if 'Unnamed' not in e}
 
-	for j in range(1):
-		seq_det = seq.to_deterministic()
-		aug_frames = [seq_det(image=im) for im in frames]
+        frames = imageio.mimread(fn)
 
-		out_fn = str.split(fn, '/')[-1]
-		out_fn = os.path.join(im_dir, re.sub('.gif', f'_{int(time.time())}.gif', out_fn))
+        for j in range(1):
+            seq_det = seq.to_deterministic()
+            aug_frames = [seq_det(image=im) for im in frames]
 
-		imageio.mimsave(out_fn, aug_frames)
+            out_fn = str.split(fn, '/')[-1]
+            out_fn = os.path.join(im_dir, re.sub('.gif', f'_{int(time.time())}.gif', out_fn))
 
-		ann_aug_batch['out_file'].append(out_fn)
+            imageio.mimsave(out_fn, aug_frames)
 
-		for k in ann_aug_batch.keys():
-			if k == 'out_file':
-				continue
+            ann_aug_batch['out_file'].append(out_fn)
 
-			ann_aug_batch[k].append(ann[k][i])	
+            for k in ann_aug_batch.keys():
+                if k == 'out_file':
+                    continue
 
-	return ann_aug_batch
+                ann_aug_batch[k].append(ann[k][i])  
+
+        return ann_aug_batch
 
 
-ann_aug = {e:[] for e in ann.keys()[1:]}
+    ann_aug = {e:[] for e in ann.keys() if 'Unnamed' not in e}
 
-for e,event in ann.groupby('single_event'):
-	print('Upsampling for ', e, '.... ')
-	for k,i in event.items():
-		if 'Unnamed' in k:
-			continue
+    for e,event in ann.groupby('single_event'):
+        print('Upsampling for ', e, '.... ')
+        for k,i in event.items():
+            if 'Unnamed' in k:
+                continue
 
-		for f in i:
-			ann_aug[k].append(f)
+            for f in i:
+                ann_aug[k].append(f)
 
-	if len(event) < limit:
-		counter = len(event)
+        if len(event) < limit:
+            counter = len(event)
 
-		event = event.sample(frac=1).reset_index(drop=True)
-		print(len(event), limit, limit - len(event))
-		for T in tqdm(range(limit - len(event))):
-			if counter > limit:
-				break
+            event = event.sample(frac=1).reset_index(drop=True)
+            print(len(event), limit, limit - len(event))
+            for T in tqdm(range(limit - len(event))):
+                if counter > limit:
+                    break
 
-			file = random.sample(list(event.out_file), 1)[0]
-			index = list(ann.out_file).index(file)
+                file = random.sample(list(event.out_file), 1)[0]
+                index = list(ann.out_file).index(file)
 
-			arg = (index, file)
-			ret = process(arg)
+                arg = (index, file)
+                ret = process(arg)
 
-			for k,i in ret.items():
-				for f in i:
-					ann_aug[k].append(f)			
+                for k,i in ret.items():
+                    for f in i:
+                        ann_aug[k].append(f)            
 
-			counter += 1
+                counter += 1
 
-ann_aug = pd.DataFrame(ann_aug)
-ann_aug.to_csv('./annotations_train_aug.csv')
+
+    ann_aug = pd.DataFrame(ann_aug)
+    ann_aug.to_csv(output_file)
--- split_dataset.py
+++ split_dataset.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
 # Parameters ---------------
-annotations_file = './annotations_gif.csv' # Annotation file produced after running 'convert_avi_to_gif.py'
+annotations_file = './annotations_new_3s.csv' # Annotation file produced after running 'convert_avi_to_gif.py'
+
+# Output CSV filenames for the training, validation and testing datasets 
+# that will be generated in this script
+annotations_train = './annotations_train_3s.csv'
+annotations_val = './annotations_val_3s.csv'
+annotations_test = './annotations_test_3s.csv'
+
 test_samples = 0.1 # i.e. 10% for testing
 val_samples = 0.1 # i.e. 10 % for validation
 # --------------------------
@@ -11,14 +18,14 @@ data = pd.read_csv(annotations_file)
 data = data.sample(frac=1).reset_index(drop=True)
 
 data_len = len(data)
-test_samples = 0.1 * data_len
-val_samples = 0.1 * data_len
+test_samples = round(0.1 * data_len)
+val_samples = round(0.1 * data_len)
 
 # Testing datasets --------------------------
 # Test samples
 test_data = data.iloc[-test_samples:, :]
 
-test_data.to_csv('./annotations_test.csv')
+test_data.to_csv(annotations_test)
 
 # Training datasets -------------------------
 train_data = data.iloc[:-test_samples, :]
@@ -26,10 +33,10 @@ train_data = data.iloc[:-test_samples, :]
 # Val samples
 val_data = train_data.iloc[-val_samples:, :]
 
-val_data.to_csv('./annotations_val.csv')
+val_data.to_csv(annotations_val)
 
 # Train samples
 train_data = train_data.iloc[:-val_samples, :]
-train_data.to_csv('./annotations_train.csv')
+train_data.to_csv(annotations_train)
 
 print(tuple(map(len, (train_data, val_data, test_data))))
